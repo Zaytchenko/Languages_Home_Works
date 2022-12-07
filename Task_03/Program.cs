302e6de77b40c21fb22097d91a2b5ffc16a26f23
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-07 17:07:21 +0200

    add Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка)
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number1 = int.Parse(Console.ReadLine());
+
+bool number2 = false;
+
+if (number1 %2 == 0) number2 = true;
+
+ Console.WriteLine("Проверка на четность: {0}", number2);
