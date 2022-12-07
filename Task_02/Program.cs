7d12836355e3bde597c89a9aa48b7992fc910d85
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-07 16:08:10 +0200

    add Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел
+Console.Clear();
+
+Console.Write("Введите первое число для сравнения: ");
+
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число для сравнения: ");
+
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число для сравнения: ");
+
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+
+if (max < number2) max = number2;
+if (max < number3) max = number3;
+    
+ Console.WriteLine($"max = {max}");
+
