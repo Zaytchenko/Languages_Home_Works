65649642f37c51371a7ea44349ce9009d09e2351
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-12 12:19:21 +0200

    add Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,25 @@
+﻿//  выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.WriteLine("Введите число N ");
+
+int N = int.Parse(Console.ReadLine());
+
+if (N < 100)
+{
+    Console.WriteLine("Третьей цифры в этом числе нет");
+}
+else
+//if (N > 999)
+{
+    while (N > 999)
+    {
+        N = N / 10;
+    }
+    N = N % 10;
+    
+    Console.WriteLine(N);
+}
+//else
+    //N = N / 10;
+
