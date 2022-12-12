dda0e296a98c9abe1219ec5b9d2b705852015dbb
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-12 11:27:29 +0200

    add Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿//  принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+
+int result = int.Parse(Console.ReadLine());
+
+result = ((result / 10)  % 10);
+Console.Write($" -> {result}");
